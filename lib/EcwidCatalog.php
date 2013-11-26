548d7fefddc5f33cb749feaabf1447e0ede75f87
Ecwid <Ecwid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-26 10:17:26 +0000

    catalog fixes
    git-svn-id: https://plugins.svn.wordpress.org/ecwid-shopping-cart/tags/2.1@810720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/EcwidCatalog.php
+++ lib/EcwidCatalog.php
@@ -100,8 +100,8 @@ class EcwidCatalog
 							$return .= sprintf(
 								'<option value="%s">%s (%s)</option>',
 								esc_attr($options_param['text']),
-								$options_param['text'],
-								$options_param['priceModifier']
+								esc_html($options_param['text']),
+								esc_html($options_param['priceModifier'])
 							);
 						}
 						$return .= '</select>';
@@ -157,15 +157,15 @@ class EcwidCatalog
 
 	function get_category($id)
 	{
-                $params = array
-                (
-                        array("alias" => "c", "action" => "categories", "params" => array("parent" => $id)),
-                        array("alias" => "p", "action" => "products", "params" => array("category" => $id)),
-                        array("alias" => "pf", "action" => "profile")
-                ); 
-                if ($id > 0) {
-                        $params[] = array('alias' => 'category', "action" => "category", "params" => array("id" => $id));
-                }
+		$params = array
+		(
+			array("alias" => "c", "action" => "categories", "params" => array("parent" => $id)),
+			array("alias" => "p", "action" => "products", "params" => array("category" => $id)),
+			array("alias" => "pf", "action" => "profile")
+		);
+		if ($id > 0) {
+			$params[] = array('alias' => 'category', "action" => "category", "params" => array("id" => $id));
+		}
 
 		$batch_result = $this->ecwid_api->get_batch_request($params);
 
