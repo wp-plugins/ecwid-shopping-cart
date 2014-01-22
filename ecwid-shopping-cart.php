9f61383141b367e8c8879e2ed117c1827c81dd19
Ecwid <Ecwid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-22 09:16:21 +0000

    Updated version in main plugin file
    git-svn-id: https://plugins.svn.wordpress.org/ecwid-shopping-cart/tags/2.3@842953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ecwid-shopping-cart.php
+++ ecwid-shopping-cart.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.ecwid.com?source=wporg
 Description: Ecwid is a free full-featured shopping cart. It can be easily integrated with any Wordpress blog and takes less than 5 minutes to set up.
 Text Domain: ecwid-shopping-cart
 Author: Ecwid Team
-Version: 2.2
+Version: 2.3
 Author URI: http://www.ecwid.com?source=wporg
 */
 
@@ -550,10 +550,10 @@ function ecwid_content_started($content)
 
 function ecwid_wrap_shortcode_content($content)
 {
-    return "<!-- Ecwid shopping cart plugin v 2.2 -->"
+    return "<!-- Ecwid shopping cart plugin v 2.3 -->"
 		   . ecwid_get_scriptjs_code()
 	       . "<div>$content</div>"
-		   . "<!-- END Ecwid Shopping Cart v 2.2 -->";
+		   . "<!-- END Ecwid Shopping Cart v 2.3 -->";
 }
 
 function ecwid_get_scriptjs_code($force_lang = null) {
