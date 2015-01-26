cb75288d51499fc26014b51845e111c9e9d1ca8a
Ecwid <Ecwid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-26 02:34:33 +0000

    Added 2.8 files
    git-svn-id: https://plugins.svn.wordpress.org/ecwid-shopping-cart/tags/2.8@1075525 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,51 @@
+	jQuery(document).ready(function() {
+	jQuery('#hide-vote-message').click(function() {
+		jQuery('#hide-vote-message').addClass('hiding');
+		jQuery.getJSON(
+			'admin-ajax.php',
+			{ action:'ecwid_hide_vote_message' }, 
+			function(data) {
+				jQuery('#hide-vote-message').removeClass('hiding')
+						.closest('div.update-nag, div.updated.fade').fadeOut();
+			}
+		);
+	});
+
+	jQuery('a.ecwid-message-hide').click(function() {
+
+		var a = this;
+		jQuery(a).css('cursor', 'wait');
+		jQuery.getJSON(
+			'admin-ajax.php',
+			{
+				action: 'ecwid_hide_message',
+				message: a.name
+			},
+			function(data) {
+				jQuery(a).closest('.ecwid-message').fadeOut();
+			}
+		);
+	});
+
+	if (location.href.match(/wp-admin\/widgets.php/)) {
+		jQuery('div[id^="widget-"]').filter('div[id*="_ecwid"]').each(function(idx, el) {
+			if (location.href.match(/wp-admin\/widgets.php\?from-ecwid=/) && el.id.match(/__i__/)) {
+				if (jQuery('.ecwid-widget').length > 0) {
+					jQuery(el).insertAfter(jQuery('.ecwid-widget:last'));
+				} else {
+					jQuery(el).prependTo(jQuery('#widget-list'));
+				}
+				jQuery('.widget-top', el).addClass('ecwid-widget-highlighted');
+
+			}
+
+			var classname = el.id.match(/widget.*ecwid(.*)-/);
+			if (classname) {
+				classname = 'ecwid-widget-' + classname[1];
+				jQuery(el).addClass('ecwid-widget')
+					.find('.widget-top')
+					.addClass(classname);
+			}
+		});
+	}
+});
