6ae932c75759a8128107c96140aa387a96f6ae98
Ecwid <Ecwid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-04 13:29:03 +0000

    3.0.2 files
    git-svn-id: https://plugins.svn.wordpress.org/ecwid-shopping-cart/tags/3.0.2@1127721 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/products-list.js
@@ -0,0 +1,245 @@
+jQuery.widget('ecwid.productsList', {
+
+	_create: function() {
+
+		this.products = {};
+		this.container = null;
+		this._prefix = 'ecwid-productsList';
+		this.sort = [];
+		this.options = {
+			max: 3,
+			debug: false
+		};
+
+
+		this.element.addClass(this._prefix);
+		this._removeInitialContent();
+		this.container = jQuery('<ul>').appendTo(this.element);
+		this._initFromHtmlData();
+		this._readSingleProducts();
+		this._onWindowResize();
+		this._render();
+
+		var self = this;
+		jQuery(window).resize(
+			ecwid_debounce(
+				function() {
+					self._onWindowResize();
+				}
+			, 200)
+		);
+	},
+
+	_render: function() {
+		var toShow = this._getProductsToShow();
+
+		for (var i in toShow) {
+			this._showProduct(this.products[toShow[i]]);
+		}
+
+		for (var id in this.products) {
+			if (toShow.indexOf(id) == -1) {
+				this._hideProduct(this.products[id]);
+			}
+		}
+
+	},
+
+	_setOption: function(key, value) {
+		this._super(key, value);
+		if (key == 'max') {
+			this.refresh();
+		}
+	},
+
+	_getProductClass: function(id) {
+		return this._prefix + '-product-' + id;
+	},
+
+	_getProductElement: function(id) {
+		return this.container.find('.' + this._getProductClass(id));
+	},
+
+	_showProduct: function(product) {
+		var existing = this._getProductElement(product.id);
+
+		if (existing.length == 0) {
+			this._renderProduct(product);
+		}
+
+
+		this._getProductElement(product.id)
+				.addClass('show')
+				.removeClass('hide')
+				.prependTo(this.container);
+	},
+
+	_hideProduct: function(product) {
+		this._getProductElement(product.id)
+			.addClass('hide')
+			.removeClass('show');
+	},
+
+	_renderProduct: function(product) {
+		var container = jQuery('<li class="' + this._getProductClass(product.id) + '">').appendTo(this.container);
+
+		if (product.link != '') {
+			container = jQuery('<a>')
+				.attr('href', product.link)
+				.attr('title', product.name)
+				.appendTo(container);
+		}
+		if (product.image) {
+			jQuery('<div class="' + this._prefix + '-image">').append('<img src="' + product.image + '">').appendTo(container);
+		} else {
+			jQuery('<div class="' + this._prefix + '-image ecwid-noimage">').appendTo(container);
+		}
+		jQuery('<div class="' + this._prefix + '-name">').append(product.name).appendTo(container);
+		jQuery('<div class="' + this._prefix + '-price ecwid-productBrowser-price">').append(product.price).appendTo(container);
+
+	},
+
+	_initFromHtmlData: function() {
+		for (var option_name in this.options) {
+			var data_name = 'ecwid-' + option_name;
+			if (typeof(this.element.data(data_name)) != 'undefined') {
+				this._setOption(option_name, this.element.data(data_name));
+			}
+		}
+	},
+
+	_removeInitialContent: function() {
+		this.originalContentContainer = jQuery('<div class="ecwid-initial-productsList-content">')
+				.data('generatedProductsList', this)
+				.append(this.element.find('>*'))
+				.insertAfter(this.element);
+	},
+
+	_readSingleProducts: function() {
+
+		var self = this;
+		var singleProductLoaded = function (container) {
+			return jQuery('.ecwid-title', container).text() != '';
+		}
+
+		jQuery('.ecwid-SingleProduct', this.originalContentContainer).each(function(idx, el) {
+			var interval = setInterval(
+					function() {
+						if (singleProductLoaded(el)) {
+							clearInterval(interval);
+							self._readSingleProduct(el);
+						}
+					},
+					500
+			);
+		});
+	},
+
+	_readSingleProduct: function(singleProductContainer) {
+		var product = {
+			name: jQuery('.ecwid-title', singleProductContainer).text(),
+			image: jQuery('.ecwid-SingleProduct-picture img', singleProductContainer).attr('src'),
+			id: jQuery(singleProductContainer).data('single-product-id'),
+			link: jQuery(singleProductContainer).data('single-product-link'),
+		}
+		if (jQuery('.ecwid-productBrowser-price .gwt-HTML', singleProductContainer).length > 0) {
+			product.price = jQuery('.ecwid-productBrowser-price .gwt-HTML', singleProductContainer).html();
+		} else {
+			product.price = jQuery('.ecwid-price', singleProductContainer).html();
+		}
+		this.addProduct(product, true);
+	},
+
+	_getProductsToShow: function() {
+		return this.sort.slice(0, this.option('max'));
+	},
+
+	_addToSort: function(id) {
+		this.sort.push(id.toString());
+	},
+
+	_triggerError: function(message) {
+		message = 'ecwid.productsList ' + message;
+		if (this.options.debug) {
+			alert(message);
+		}
+		console.log(message);
+	},
+
+	_destroy: function() {
+		this.element.removeClass('.' + this._prefix).find('>*').remove();
+		this.element.append(this.originalContentContainer.find('>*'));
+		this.originalContentContainer.data('generatedProductsList', null);
+		this.originalContentContainer = null;
+		this._superApply(arguments);
+	},
+
+	refresh: function() {
+		this._render();
+	},
+
+	addProduct: function(product, forceRender) {
+		if (typeof(product.id) == 'undefined') {
+			this._triggerError('addProduct error: product must have id');
+		}
+
+		if (typeof this.products[product.id] != 'undefined') {
+			return;
+		}
+
+		this.products[product.id] = jQuery.extend(
+				{}, {
+					id: 0,
+					name: 'no name',
+					image: '',
+					link: '',
+					price: '',
+					toString: function() {return this.name;}
+				},
+				product
+		);
+
+		this._addToSort(product.id);
+
+		if (forceRender) {
+			this._render();
+		}
+	},
+
+	_onWindowResize: function() {
+		if (this.element.width() < 150) {
+			this.element.addClass('width-s').removeClass('width-m width-l');
+		} else if (this.element.width() < 300) {
+			this.element.addClass('width-m').removeClass('width-s width-l');
+		} else {
+			this.element.addClass('width-l').removeClass('width-s width-m');
+		}
+	}
+});
+
+
+// Debounce function from http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+var ecwid_debounce = function (func, threshold, execAsap) {
+
+	var timeout;
+
+	return function debounced () {
+		var obj = this, args = arguments;
+		function delayed () {
+			if (!execAsap) {
+				func.apply(obj, args);
+			}
+			timeout = null;
+		};
+
+		if (timeout)
+			clearTimeout(timeout);
+		else if (execAsap)
+			func.apply(obj, args);
+
+		timeout = setTimeout(delayed, threshold || 100);
+	};
+
+}
+
+jQuery('.ecwid-productsList').trigger('ecwidOnWindowResize');
---
+++ js/recently-viewed.js
@@ -0,0 +1,77 @@
+wpCookies.set('test_ecwid_shopping_cart_recently_products_cookie', 'test_ecwid_shopping_cart_cookie_value', { path: '/' });
+if (wpCookies.get('test_ecwid_shopping_cart_recently_products_cookie') != 'test_ecwid_shopping_cart_cookie_value') {
+	// Cookies do not work, we do nothing
+	exit;
+}
+
+jQuery.widget('ecwid.recentlyViewedProducts', jQuery.ecwid.productsList, {
+	_justAdded: null,
+
+	_create: function() {
+		this._superApply(arguments);
+
+		var self = this;
+		Ecwid.OnPageLoaded.add(
+			function(page) {
+
+				self._justAdded = null;
+
+				if (page.type == 'PRODUCT') {
+					var product = {
+						id: page.productId.toString(),
+						name: page.name
+					}
+
+					setTimeout(function() {
+						self.addViewedProduct(product);
+					}, 500);
+				}
+			}
+		);
+	},
+
+	addViewedProduct: function(product) {
+		product.image = jQuery('.ecwid-productBrowser-details-thumbnail .gwt-Image').attr('src');
+		product.link = window.location.href;
+		if (jQuery('.ecwid-productBrowser-price .ecwid-productBrowser-price-value').length > 0) {
+			product.price = jQuery('.ecwid-productBrowser-price .ecwid-productBrowser-price-value').html();
+		} else {
+			product.price = jQuery('.ecwid-productBrowser-price').html();
+		}
+
+		if (typeof this.products[product.id] == 'undefined') {
+			this._justAdded = product.id;
+			this.addProduct(product);
+		} else {
+			this.sort.splice(this.sort.indexOf(product.id), 1);
+			this._addToSort(product.id);
+		}
+
+		this._render();
+	},
+
+	_getProductsToShow: function() {
+		// copy array using slice
+		var sort = this.sort.slice();
+
+		if (this._justAdded) {
+			sort.splice(sort.indexOf(this._justAdded), 1);
+		}
+
+		if (sort.length > this.options.max && jQuery('.ecwid-productBrowser-ProductPage').length > 0) {
+			var currentProductId = jQuery('.ecwid-productBrowser-ProductPage').attr('class').match(/ecwid-productBrowser-ProductPage-(\d+)/);
+
+			if (sort.indexOf(currentProductId[1]) != -1) {
+				sort.splice(
+					sort.indexOf(
+						currentProductId[1]
+					), 1
+				);
+			}
+		}
+
+		return sort.reverse().slice(0, this.option('max')).reverse();
+	}
+});
+
+jQuery('.ecwid-recently-viewed-products').recentlyViewedProducts();
